f441c63249c198e2c1d517da8aa4c8d106707e69
ekomninos <komninos@kth.se>
2015-07-17 11:51:44 +0200

    Create jtag_vip.svh

---
+++ sv/jtag_vip.svh
@@ -0,0 +1,10 @@
+`include "jtag_sequence_item.svh"
+`include "jtag_config.svh"
+
+`include "jtag_sequence_lib.svh"
+
+`include "jtag_sequencer.svh"
+`include "jtag_driver.svh"
+`include "jtag_agent.svh"
+
+`include "jtag_env.svh"

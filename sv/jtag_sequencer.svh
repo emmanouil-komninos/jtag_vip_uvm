95208048ff1bd73b317d58a9e24613ad7a8081d4
ekomninos <komninos@kth.se>
2015-07-17 11:14:36 +0200

    Create jtag_sequencer.svh

---
+++ sv/jtag_sequencer.svh
@@ -0,0 +1,15 @@
+`ifndef JTAG_SEQUENCER__SVH
+ `define JTAG_SEQUENCER__SVH
+
+class jtag_sequencer extends uvm_sequencer #(jtag_send_packet);
+  
+  `uvm_component_utils(jtag_sequencer)
+
+  function new (string name, uvm_component parent);
+    super.new(name,parent);
+  endfunction // new
+  
+endclass // jtag_sequencer
+
+
+`endif

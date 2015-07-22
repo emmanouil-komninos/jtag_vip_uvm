a616f72ca5a1abce79bec0efb02f1162b7b2bbd0
Emmanouil Komninos <komninos@kth.se>
2015-07-22 15:55:10 +0200

    Update jtag_sequence_item.svh
    added instr_sz and data_sz to be used by driver in shift phase

--- sv/jtag_sequence_item.svh
+++ sv/jtag_sequence_item.svh
@@ -4,12 +4,19 @@
 // sequence items
 class jtag_send_packet extends uvm_sequence_item;
   
-  rand logic [0:3] instr_reg;
+  rand logic [0:3] instr;
   rand logic [0:7] data;
+  rand int instr_sz;
+  rand int data_sz;
 
+  constraint c_instr_sz {instr_sz == $size(instr);}
+  constraint c_data_sz {data_sz == $size(data);}
+  
   `uvm_object_utils_begin(jtag_send_packet)
-  `uvm_field_int(instr_reg, UVM_ALL_ON)
+  `uvm_field_int(instr, UVM_DEFAULT)
   `uvm_field_int(data, UVM_DEFAULT)
+  `uvm_field_int(instr_sz, UVM_DEFAULT)
+  `uvm_field_int(data_sz, UVM_DEFAULT)
   `uvm_object_utils_end
 
     function new (string name = "jtag_send_packet");
@@ -22,7 +29,7 @@ class jtag_idcode extends jtag_send_packet;
 
   `uvm_object_utils(jtag_idcode)
 
-  constraint c_instr_reg { instr_reg == 4'b0010;}
+  constraint c_instr { instr == 4'b0010;}
   
   function new (string name = "jtag_idcode");
     super.new(name);

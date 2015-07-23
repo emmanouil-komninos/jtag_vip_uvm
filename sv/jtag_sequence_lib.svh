5081a3a8d184c9c1c99a65e1ba2c77384b9b0d56
Emmanouil Komninos <komninos@kth.se>
2015-07-23 12:21:31 +0200

    Update jtag_sequence_lib.svh
    added sequence with rand delay. added constraint for delay to jtag_simple_sequence

--- sv/jtag_sequence_lib.svh
+++ sv/jtag_sequence_lib.svh
@@ -29,19 +29,55 @@ class jtag_simple_sequence extends uvm_sequence #(jtag_send_packet);
     if (!uvm_config_db#(test_configuration)::get(null,"","test_cfg",test_cfg))
       begin
         `uvm_info("JTAG_SIMPLE_SEQUENCE","No test configuration exists in config db", UVM_LOW)
-        repeat_cnt = 3;
+        repeat_cnt = 100;
       end
     
     repeat(repeat_cnt)
-      begin 
+      begin
+        // `uvm_do_with(req, {req.delay == 0;}) expanded below
         req = jtag_send_packet::type_id::create("req");
         start_item(req);       
-        if (!req.randomize())
+        if (!req.randomize() with {req.delay == 0;})
           `uvm_fatal("JTAG_SIMPLE_SEQ", "Failed on randomization of req")   
         finish_item(req);
+        
+        // if response is returned from the driver with item_done(req)
+        // get_response(rsp);
+
       end
+    
   endtask // body
   
 endclass // jtag_simple_sequence
+
+class jtag_simple_sequence_with_rand_delay extends jtag_simple_sequence;
+
+  function new (string name = "jtag_simple_sequence_with_rand_delay");
+    super.new(name);
+  endfunction // new
+  
+  virtual task body();
+    int repeat_cnt;
+    
+    if (!uvm_config_db#(test_configuration)::get(null,"","test_cfg",test_cfg))
+      begin
+        `uvm_info("JTAG_SIMPLE_SEQUENCE_WITH_RAND_DELAY","No test configuration exists in config db", UVM_LOW)
+        repeat_cnt = 100;
+      end
+    
+    repeat(repeat_cnt)
+      begin
+        // `uvm_do(req) expanded below
+        req = jtag_send_packet::type_id::create("req");
+        start_item(req);       
+        if (!req.randomize())
+          `uvm_fatal("JTAG_SIMPLE_SEQ_WITH_RAND_DELAY", 
+                     "Failed on randomization of req")   
+        finish_item(req);
+      end
+    
+  endtask // body
+  
+endclass // jtag_simple_sequence_with_rand_delay
 
 `endif

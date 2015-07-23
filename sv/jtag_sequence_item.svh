809f8b180434be0606547a6567431e3c383a0e1c
Emmanouil Komninos <komninos@kth.se>
2015-07-23 12:19:34 +0200

    Update jtag_sequence_item.svh
    added enum type for instr

--- sv/jtag_sequence_item.svh
+++ sv/jtag_sequence_item.svh
@@ -4,11 +4,13 @@
 // sequence items
 class jtag_send_packet extends uvm_sequence_item;
   
-  rand logic [0:3] instr;
+  rand jtag_instr_registers instr;
   rand logic [0:7] data;
   rand int instr_sz;
   rand int data_sz;
-
+  
+  rand bit [0:3] delay;
+  
   constraint c_instr_sz {instr_sz == $size(instr)-1;}
   constraint c_data_sz {data_sz == $size(data)-1;}
   
@@ -17,6 +19,7 @@ class jtag_send_packet extends uvm_sequence_item;
   `uvm_field_int(data, UVM_DEFAULT)
   `uvm_field_int(instr_sz, UVM_DEFAULT)
   `uvm_field_int(data_sz, UVM_DEFAULT)
+  `uvm_field_int(delay, UVM_DEFAULT)
   `uvm_object_utils_end
 
     function new (string name = "jtag_send_packet");
@@ -30,6 +33,7 @@ class jtag_idcode extends jtag_send_packet;
   `uvm_object_utils(jtag_idcode)
 
   constraint c_instr { instr == 4'b0010;}
+  constraint c_delay { delay == 0;}
   
   function new (string name = "jtag_idcode");
     super.new(name);

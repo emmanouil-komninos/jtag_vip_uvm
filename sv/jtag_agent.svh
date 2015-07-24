14f8d5d5b1d349dd53b5c8d540a46a1bf31d2b1f
Emmanouil Komninos <komninos@kth.se>
2015-07-24 14:32:25 +0200

    Update jtag_agent.svh
    added collector and monitor

--- sv/jtag_agent.svh
+++ sv/jtag_agent.svh
@@ -2,9 +2,12 @@
  `define JTAG_AGENT__SVH
 
 class jtag_agent extends uvm_agent;
- 
+  
   jtag_driver jtag_drv;
   jtag_sequencer jtag_seqr;
+
+  jtag_collector jtag_col;
+  jtag_monitor jtag_mon;
   
   jtag_agent_config jtag_agent_cfg;
 
@@ -12,9 +15,9 @@ class jtag_agent extends uvm_agent;
   `uvm_field_object(jtag_agent_cfg, UVM_DEFAULT)
   `uvm_component_utils_end
     
-  function new (string name, uvm_component parent);
-    super.new(name,parent);
-  endfunction // new
+    function new (string name, uvm_component parent);
+      super.new(name,parent);
+    endfunction // new
 
   function void build_phase (uvm_phase phase);
     super.build_phase(phase);
@@ -24,17 +27,22 @@ class jtag_agent extends uvm_agent;
         jtag_agent_cfg = jtag_agent_config::type_id::create("jtag_agent_cfg");
         if (!jtag_agent_cfg.randomize())
           `uvm_fatal("JTAG_AGENT_FATAL", "Randomization of jtag_agent_cfg failed")
+        // jtag_agent_cfg.print();
+        // jtag_agent_cfg.jtag_drv_cfg.print();
       end
     else
       `uvm_info("JTAG_AGENT_INFO", " Agent used auto config", UVM_LOW)
-      
-      if (jtag_agent_cfg.is_active == UVM_ACTIVE)
-        begin
-          `uvm_info("JTAG_AGENT_INFO", "Agent is active... building drv and seq", UVM_LOW)
-          uvm_config_db#(uvm_object)::set(this,"jtag_drv","jtag_drv_cfg", jtag_agent_cfg.jtag_drv_cfg);
-          jtag_drv = jtag_driver::type_id::create("jtag_drv",this);
-          jtag_seqr = jtag_sequencer::type_id::create("jtag_seqr",this);
-        end
+    
+    if (jtag_agent_cfg.is_active == UVM_ACTIVE)
+      begin
+        `uvm_info("JTAG_AGENT_INFO", "Agent is active... building drv and seq", UVM_LOW)
+        uvm_config_db#(uvm_object)::set(this,"jtag_drv","jtag_drv_cfg", jtag_agent_cfg.jtag_drv_cfg);
+        jtag_drv = jtag_driver::type_id::create("jtag_drv",this);
+        jtag_seqr = jtag_sequencer::type_id::create("jtag_seqr",this);
+      end
+
+    jtag_col = jtag_collector::type_id::create("jtag_col",this);
+    jtag_mon = jtag_monitor::type_id::create("jtag_mon",this);    
     
   endfunction // build_phase
 

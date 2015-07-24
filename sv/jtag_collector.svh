871ae353bbe8976f0f4cca16d684c4ecc0883378
Emmanouil Komninos <komninos@kth.se>
2015-07-24 17:09:12 +0200

    Update jtag_collector.svh

--- sv/jtag_collector.svh
+++ sv/jtag_collector.svh
@@ -5,6 +5,8 @@
 class jtag_collector extends uvm_component;
   
   jtag_vif jtag_vif_col;
+
+  uvm_analysis_port #(jtag_receive_packet) item_collected_port;
   
   `uvm_component_utils(jtag_collector)
 
@@ -12,9 +14,19 @@ class jtag_collector extends uvm_component;
     super.new(name, parent);
   endfunction // new
 
+  virtual function void build_phase (uvm_phase phase);
+    super.build_phase(phase);
+    item_collected_port = new("item_collected_port",this);
+  endfunction // build_phase
+
+  virtual function void connect_phase (uvm_phase phase);
+    super.connect_phase(phase);
+    
+    if (!uvm_config_db#(jtag_vif)::get(this, get_full_name(), "jtag_virtual_if", jtag_vif_col))
+      `uvm_fatal("JTAG_COLLECTOR", "Virtual interface is null")
+    
+  endfunction // connect_phase
   
 endclass // jtag_collector
-
-
 
 `endif

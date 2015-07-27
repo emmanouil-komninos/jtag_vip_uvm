25bfa99510361e6135bdb7d482825c946f5abac1
Emmanouil Komninos <komninos@kth.se>
2015-07-27 16:51:05 +0200

    Update jtag_driver.svh

--- sv/jtag_driver.svh
+++ sv/jtag_driver.svh
@@ -1,16 +1,14 @@
 `ifndef JTAG_DRIVER__SVH
  `define JTAG_DRIVER__SVH
 
-typedef enum {RESET, IDLE, SELECT_DR, SELECT_IR, CAPTURE, SHIFT, EXIT, EXIT2, PAUSE, UPDATE} state;
-
 class jtag_driver extends uvm_driver #(jtag_send_packet, jtag_receive_packet);
   
   // configuration component for the driver
   jtag_driver_config jtag_drv_cfg;
   
   // no automation for the following 
-  state next_state = IDLE;
-  state current_state = RESET;
+  tap_state next_state = IDLE;
+  tap_state current_state = RESET;
   bit        exit = 0;
   jtag_send_packet temp_req;
   
@@ -43,27 +41,32 @@ class jtag_driver extends uvm_driver #(jtag_send_packet, jtag_receive_packet);
    
     `uvm_info("JTAG_DRIVER_INFO","Driver Connect phase",UVM_LOW)
     
-    if(!uvm_config_db#(jtag_vif)::get(this, get_full_name(), "jtag_virtual_if", jtag_vif_drv))
+    if(!uvm_config_db#(jtag_vif)::get(null, get_full_name(), "jtag_virtual_if", jtag_vif_drv))
       `uvm_fatal("JTAG_DRIVER_FATAL", {"VIF must be set for: ", get_full_name()})
+    else
+      `uvm_info("JTAG_DRIVER_INFO", {"VIF is set for: ", get_full_name()},UVM_LOW )
       
   endfunction // connect_phase
 
   task run_phase (uvm_phase phase);
     if (jtag_vif_drv == null)
       begin
-        `uvm_error("JTAG_DRIVER_ERROR", {"VIF must be set for: ", get_full_name(), ".jtag_vif_drv"})
-        `uvm_fatal("JTAG_DRIVER_INFO", "NO VIF")
+        `uvm_fatal("JTAG_DRIVER_FATAL", {"VIF must be set for: ", get_full_name()})
       end
     else
       `uvm_info("JTAG_DRIVER_INFO", " Driver used if from config db", UVM_LOW)
-
+    
     // time consuming part
     while(1)
       begin
-        seq_item_port.get_next_item(req);
-        
+        seq_item_port.get_next_item(req); // blocking
+
+        // new pointer for each response.... 
+        // maybe the sequence is draining rsp slower than the driver provides them
         rsp = jtag_receive_packet::type_id::create("rsp");
-        // in the sequence, when calling get_response(), we can optionally provide the transaction_id of the req
+        
+        // in the sequence, when calling get_response(), 
+        // we can optionally provide the transaction_id of the req to pick up the specific rsp
         rsp.set_id_info(req);
         
         phase.raise_objection(this,"Jtag Driver raised objection");
@@ -95,9 +98,7 @@ class jtag_driver extends uvm_driver #(jtag_send_packet, jtag_receive_packet);
         repeat (15) @jtag_vif_drv.drv_ck;
         // uvm_test_done.drop_objection(this);
       end
-  endtask // all_dropped  
-  
-  state current_dr_state = IDLE;
+  endtask // all_dropped
   
   extern task dr_seq();
   extern task ir_seq();
@@ -159,30 +160,30 @@ function void jtag_driver::drive_tms_dr();
         // this.next_state = SELECT_DR;
         jtag_vif_drv.tms = 1;
       end
-    SHIFT:
+    SHIFT_DR:
       begin
         if (this.temp_req.data_sz > 0)
           begin
-            // this.next_state = SHIFT;
+            // this.next_state = SHIFT_DR;
             jtag_vif_drv.tdi = this.temp_req.data[this.temp_req.data_sz];
             this.temp_req.data_sz--;
             capture_tdo = 1;
           end
         else
           begin
-            // this.next_state = EXIT;
+            // this.next_state = EXIT_DR;
             // drive last bit to tdi
             jtag_vif_drv.tdi = this.temp_req.data[this.temp_req.data_sz];
             jtag_vif_drv.tms = 1;
           end
       end
-    EXIT:
+    EXIT_DR:
       begin
-        // this.next_state = UPDATE;
+        // this.next_state = UPDATE_DR;
         jtag_vif_drv.tms = 1;
         capture_tdo = 0;
       end
-    UPDATE:
+    UPDATE_DR:
       begin
         // this.next_state = IDLE;
         this.exit = 1;
@@ -197,7 +198,14 @@ endfunction // drive_tms_dr
 // compute tms based on current state
 function void jtag_driver::drive_tms_ir();
   
+  static bit capture_tdo = 0;
+  
   this.exit = 0;
+  
+  if (capture_tdo)
+    begin
+        rsp.instr = {jtag_vif_drv.tdo, rsp.instr[3:1]};
+    end
   
   jtag_vif_drv.tms = 0;
   case (this.current_state)
@@ -211,28 +219,28 @@ function void jtag_driver::drive_tms_ir();
         // this.next_state = SELECT_IR;
         jtag_vif_drv.tms = 1;
       end
-    SHIFT:
+    SHIFT_IR:
       begin
         if (this.temp_req.instr_sz > 0)
           begin
-            // this.next_state = SHIFT;
+            // this.next_state = SHIFT_IR;
             jtag_vif_drv.tdi = this.temp_req.instr[this.temp_req.instr_sz];
             this.temp_req.instr_sz--;
           end
         else
           begin
-            // this.next_state = EXIT;
+            // this.next_state = EXIT_IR;
             // drive last bit to tdi
             jtag_vif_drv.tdi = this.temp_req.instr[this.temp_req.instr_sz];
             jtag_vif_drv.tms = 1;
           end
       end
-    EXIT:
+    EXIT_IR:
       begin
-        // this.next_state = UPDATE;
+        // this.next_state = UPDATE_IR;
         jtag_vif_drv.tms = 1;
       end
-    UPDATE:
+    UPDATE_IR:
       begin
         // this.next_state = IDLE;
         this.exit = 1;
@@ -262,47 +270,78 @@ function void jtag_driver::compute_state(bit tms);
         if(tms == 1) 
           this.next_state = SELECT_IR;
         else
-          this.next_state = CAPTURE;
+          this.next_state = CAPTURE_DR;
       end
     SELECT_IR: 
       begin
         if(tms == 1)
           this.next_state = RESET;
         else
-          this.next_state = CAPTURE;
+          this.next_state = CAPTURE_IR;
       end
-    CAPTURE: 
+    CAPTURE_DR: 
       begin
         if(tms == 1)
-          this.next_state = EXIT;
+          this.next_state = EXIT_DR;
         else
-          this.next_state = SHIFT;
+          this.next_state = SHIFT_DR;
       end
-    SHIFT: 
+    CAPTURE_IR: 
       begin
         if(tms == 1)
-          this.next_state = EXIT;
-      end
-    EXIT:
-      begin
-        if(tms == 1)
-          this.next_state = UPDATE;
+          this.next_state = EXIT_IR;
         else
-          this.next_state = PAUSE;
+          this.next_state = SHIFT_IR;
       end
-    PAUSE:
+    SHIFT_DR: 
       begin
         if(tms == 1)
-          this.next_state = EXIT2;
+          this.next_state = EXIT_DR;
       end
-    EXIT2:
+    SHIFT_IR: 
       begin
         if(tms == 1)
-          this.next_state = UPDATE;
+          this.next_state = EXIT_IR;
+      end
+    EXIT_DR:
+      begin
+        if(tms == 1)
+          this.next_state = UPDATE_DR;
         else
-          this.next_state = SHIFT;
+          this.next_state = PAUSE_DR;
       end
-    UPDATE:
+    EXIT_IR:
+      begin
+        if(tms == 1)
+          this.next_state = UPDATE_IR;
+        else
+          this.next_state = PAUSE_IR;
+      end
+    PAUSE_DR:
+      begin
+        if(tms == 1)
+          this.next_state = EXIT2_DR;
+      end
+    PAUSE_IR:
+      begin
+        if(tms == 1)
+          this.next_state = EXIT2_IR;
+      end
+    EXIT2_DR:
+      begin
+        if(tms == 1)
+          this.next_state = UPDATE_DR;
+        else
+          this.next_state = SHIFT_DR;
+      end
+    EXIT2_IR:
+      begin
+        if(tms == 1)
+          this.next_state = UPDATE_IR;
+        else
+          this.next_state = SHIFT_IR;
+      end
+    UPDATE_DR, UPDATE_IR:
       begin
         if(tms == 1)
           this.next_state = SELECT_DR;

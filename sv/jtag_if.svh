741b745fa81002e5ca9c74b6a14c2f84d31bd50a
Emmanouil Komninos <komninos@kth.se>
2015-09-07 14:13:25 +0200

    Update jtag_if.svh
    added proxy

--- sv/jtag_if.svh
+++ sv/jtag_if.svh
@@ -1,6 +1,8 @@
 `ifndef JTAG_IF__SVH
  `define JTAG_IF__SVH
 
+import jtag_tb_pkg::*;
+
 interface jtag_if (input bit tck);
   
   logic tdi;
@@ -9,6 +11,9 @@ interface jtag_if (input bit tck);
   logic trst;
   logic vref;
   logic gnd;
+  logic test = 'z;
+
+  assign test_bus = test;
   
   clocking tb_ck @(posedge tck);
     output tdi;
@@ -16,10 +21,21 @@ interface jtag_if (input bit tck);
     input  negedge tdo;
     input  vref;
     input  gnd;
+    output test;
+    
   endclocking // tb_ck
 
-  modport jtag_tb_mod (clocking tb_ck);
-      
+   modport jtag_tb_mod (clocking tb_ck);
+
+    //  // proxy class extention and instantiation
+     class if_proxy extends jtag_tb_pkg::jtag_if_proxy;
+       virtual function void set_tdi(bit tdi);
+         tb_ck.tdi <= tdi;
+       endfunction // set_tdi
+     endclass // jtag_if_proxy
+
+    if_proxy proxy = new();
+    
 endinterface // jtag_if
 
 `endif
